1342dfd4aeb43d5a8a261b52e87749c2fca08ab6
YUNJUNG KIM <kyjekffo1105@yahoo.com>
2020-04-01 00:26:36 +0000

    Done.

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,5 +1,8 @@
 require 'pry'
 
 def reverse_each_word(string)
- (string.split(" ").collect {|element| element.reverse}).join(" ")
+  # (string.split(" ").collect {|element| element.reverse}).join(" ")
+  # string.split(" ").collect {|element| element.reverse}.join(" ")
+
+  string.split.collect {|word| word.reverse}.join(" ")
 end
