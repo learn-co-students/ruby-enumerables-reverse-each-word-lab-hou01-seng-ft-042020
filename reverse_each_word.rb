50bc4e93bcc2f85a1f605cff2ae1734e536cd27d
Richard Chea <rchea817@gmail.com>
2020-04-12 00:59:13 +0000

    Done.

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,8 +1,16 @@
 require "pry"
+#Turn each word in sentence to an array, reverse the array, change into a string, print string
+# def reverse_each_word(sentence)
+#   new_array = sentence.split(" ")
+#   reversed_array = []
+#   new_array.each { |word| reversed_array << word.reverse!}
+#   return reversed_array.join(" ")
+# end
+
 def reverse_each_word(sentence)
-  array = []
+  new_array = []
   sentence.split.collect do |words|
-    array << words.reverse
+    new_array << words.reverse
   end
-  array.join(" ")
+  new_array.join(" ")
 end
