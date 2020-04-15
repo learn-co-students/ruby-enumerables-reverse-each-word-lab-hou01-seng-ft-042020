f4f2b3a0b77bf79d3a366cc44eb9207afaa5c8c4
edwin calvillo <calvillo.edwin@gmail.com>
2020-04-15 03:28:05 +0000

    Done.

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,11 +1,5 @@
 def reverse_each_word (string)
   array = string.split(/ /)
-  array_new = []
-  array.each {|a| array_new << a.reverse }
-  p array_new.join(" ")
-
-
-  array = string.split(/ /)
   array_new = array.collect {|a| a.reverse }
-  p array_new.join(" ")
+  return array_new.join(" ")
 end
